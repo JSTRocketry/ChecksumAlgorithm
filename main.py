7f1d7c2dd6f0f8984268ae04b7ff820e519a4d2c
Jordan <jstrocketry@gmail.com>
2018-06-25 21:18:47 -0400

    new checksum algorithm - simplified and reliable

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-LEAD = "CA"
-MSB_FORMAT = "00"
-LSB_FORMAT = "01"
-CHECKSUM_LEAD_TAG = LEAD + MSB_FORMAT + LSB_FORMAT
-
 def checksum(data):
     storedData = " ".join("{:02X}".format(ord(c)) for c in data)
     splitData = storedData.split()
     hexData = []
-    toReturn = CHECKSUM_LEAD_TAG + "%02X" % int(hex(len(splitData)), 16)
     for i in splitData:
         hexData.append(int(i, 16))
-        toReturn += "%s" % i
-    addedData = 0x00 + 0x01 + int(hex(len(splitData)), 16)
+    cs = 0
     for x in hexData:
-        addedData += x
-    if(addedData > 0xFF):
-        addedData %= 0x100
-    xorData = hex(addedData ^ 0xFF)
-    toReturn += "%02X" % int(xorData, 16)
+        cs ^= x
+    toReturn = "{:02X}".format(cs)
     return toReturn
 
 def compare(cs1, cs2):
@@ -28,12 +18,10 @@ def compare(cs1, cs2):
     return False
 
 def main():
-    testData = "Bye"
-    cs = "CC000103427965DB"
-    cs2 = checksum(testData)
-    if(compare(cs, cs2)):
-        print("True")
-    else:
-        print("False")
+    testData = "@{PA:56.2;TS:5000}@"
+    chksum = "3E"
+    dataCS = checksum(testData)
+    print(dataCS)
+    print(compare(chksum, dataCS))
 
 main()
